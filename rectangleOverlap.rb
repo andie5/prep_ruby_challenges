d26995b4bebd8a654603e7eb3e3bfc120c10a604
Andie <andie5@users.noreply.github.com>
2016-06-28 21:34:05 +0100

    Adding <= to each if condition to deal with the case of the rectangles touching which is not classed as overlapping

--- rectangleOverlap.rb
+++ rectangleOverlap.rb
@@ -3,28 +3,27 @@
 def overlap(recOne, recTwo)
 
   left1 = recOne[0]
-  print "left1 #{left1}\n"
+  # print "left1 #{left1}\n"
   right1 = recOne[1]
-  print "right1 #{right1}\n"
+  # print "right1 #{right1}\n"
   left2 = recTwo[0]
-  print "left2 #{left2}\n"
+  # print "left2 #{left2}\n"
   right2 = recTwo[1]
-  print "right2 #{right2}\n"
-
+  # print "right2 #{right2}\n"
 
   #  If one rectangle is on left side of other
-    if (left1[0] > right2[0] || left2[0] > right1[0])
-      return print "false"
+    if (left1[0] >= right2[0] || left2[0] >= right1[0])
+      return print "false\n"
     end
 
   #  If one rectangle is above other
-    if (right1[1] < left2[1] || right2[1] < left1[1])
-        return print "false"
+    if (right1[1] <= left2[1] || right2[1] <= left1[1])
+        return print "false\n"
     end
 
-    print "1st if #{left1[0]} > #{right2[0]} or #{left2[0]} > #{right1[0]} \n"
-    print "2nd if #{right1[1]} < #{left2[1]} or #{right2[1]} < #{left1[1]}\n"
-    return print "true"
+    # print "1st if #{left1[0]} > #{right2[0]} or #{left2[0]} > #{right1[0]} \n"
+    # print "2nd if #{right1[1]} < #{left2[1]} or #{right2[1]} < #{left1[1]}\n"
+    return print "true\n"
 end
 
 overlap([ [0,0],[3,3] ], [ [1,1],[4,5] ] )
